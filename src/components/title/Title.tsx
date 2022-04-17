ab9477718173b104f64b7dda2189d7da15469f08
febhost32 <feb.host32@hotmail.com>
2022-04-17 22:55:12 +0800

    First Commit

---
+++ src/components/title/Title.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.css";
+
+const Title: React.FunctionComponent = () => {
+    
+    return (
+    <div className="title">
+        <h1>Febrian Pratama Putra</h1>
+        <h2>Web Developer, aspiring to be the Software Engineer</h2>
+    </div>
+    )
+}
+
+
+export default Title;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MainPage.css";
+import Title from "../components/title/Title";
+
+function MainPage() {
+
+
+        return (
+            <div className="page-container">
+                <Title />
+            </div>
+        )
+
+}
+
+export default MainPage;
